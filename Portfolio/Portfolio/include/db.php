826113a87d847134a3f73754f197d7be074e231e
mob1000mob <160032318+mob1000mob@users.noreply.github.com>
2024-06-15 10:45:27 +0000

    git commit -m "Update db.php to connect to Azure MySQL database using environment variables"

--- Portfolio/Portfolio/include/db.php
+++ Portfolio/Portfolio/include/db.php
@@ -1,7 +1,16 @@
 <?php
-$db=mysqli_connect("localhost","root","","iportfolio");
-//if($db){
-//    echo "databse is connected !";
-//}else{
-//    echo "something is wrong with database !";
-//
+// Database connection details
+$servername = "group6server.mysql.database.azure.com";
+$username = "group6";
+$password = "ThePortfolioHub6";
+$dbname = "group6portfolio";
+
+// Create connection
+$db = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Database is connected!";
+?>
