46ee4b244ab40d20f6e3ec1489c571c169facd79
mob1000mob <160032318+mob1000mob@users.noreply.github.com>
2024-06-16 17:02:15 +0800

    Update db.php

--- Portfolio/Portfolio/include/db.php
+++ Portfolio/Portfolio/include/db.php
@@ -1,16 +1,7 @@
 <?php
-// Database connection details
-$servername = "group6server.mysql.database.azure.com";
-$username = "group6";
-$password = "ThePortfolioHub6";
-$dbname = "group6portfolio";
-
-// Create connection
-$db = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Database is connected!";
-?>
+$db=mysqli_connect("localhost","root","","portfolio");
+//if($db){
+//    echo "databse is connected !";
+//}else{
+//    echo "something is wrong with database !";
+//
